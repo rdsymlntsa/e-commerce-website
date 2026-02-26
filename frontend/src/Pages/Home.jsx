76b82282aeac1b99df1c48820028b5e3dd6f91fb
rdsymlntsa <rdsymlntsa@gmail.com>
2026-02-26 21:06:38 +0530

    Built the new arrivals section , made it scrollable using mouse

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
+import NewArrivals from "../components/Products/NewArrivals";
 const Home=()=>{
   return <>
   <Hero />
   <GenderCollectionSection/>
+  <NewArrivals />
   </>
 }
 export default Home;
