e4576792e1a25006550f50bfa603718eb480c095
rdsymlntsa <rdsymlntsa@gmail.com>
2026-03-06 17:47:05 +0530

    Finished building UI for best seller section

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrivals from "../components/Products/NewArrivals";
+import ProductDetails from "../components/Products/ProductDetails";
+
 const Home=()=>{
   return <>
   <Hero />
   <GenderCollectionSection/>
   <NewArrivals />
+  <h3 className="text-3xl text-center mb-4">Best Seller</h3>
+  <ProductDetails/>
   </>
 }
 export default Home;
---
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -0,0 +1,146 @@
+import { useState, useEffect } from "react";
+
+const selectedProduct = {
+  name: "Stylish Jacket",
+  price: 120,
+  originalPrice: 150,
+  description: "This is a stylish jacket perfect for any occasion",
+  brand: "Fashion Brand",
+  material: "Leather",
+  sizes: ["S", "M", "L", "XL"],
+  colors: ["Red", "Black"],
+  images: [
+    {
+      url: "https://picsum.photos/500/500?random=1",
+      altText: "Stylish Jacket 1",
+    },
+    {
+      url: "https://picsum.photos/500/500?random=2",
+      altText: "Stylish Jacket 2",
+    },
+  ],
+};
+
+const ProductDetails = () => {
+  const [mainImage, setMainImage] = useState("");
+
+  useEffect(() => {
+    if (selectedProduct?.images?.length > 0) {
+      setMainImage(selectedProduct.images[0].url);
+    }
+  }, [selectedProduct]);
+
+  return (
+    <div className="p-6">
+      <div className="p-8 max-w-6xl bg-white rounded-lg">
+        <div className="flex flex-col md:flex-row">
+          <div className="hidden md:flex flex-col space-y-4 mr-6">
+            {selectedProduct.images.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={image.altText || `Thumbnail ${index}`}
+                className={`w-20 h-20 object-cover border rounded-lg ${mainImage === image.url ? "border-black" : "border-gray-300"}`}
+                onClick={() => setMainImage(image.url)}
+              />
+            ))}
+          </div>
+          <div className="md:w-1/2">
+            <div className="mb-4">
+              <img
+                src={mainImage}
+                alt="Main Product"
+                className="w-full h-auto object-cover rounded-lg"
+              />
+            </div>
+          </div>
+          <div className="md:hidden flex overflow-x-scroll space-x-4 mb-4">
+            {selectedProduct.images.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={image.altText || `Thumbnail ${index}`}
+                className={`w-20 h-20 object-cover border rounded-lg ${mainImage === image.url ? "border-black" : "border-gray-300"}`}
+                onClick={() => setMainImage(image.url)}
+              />
+            ))}
+          </div>
+
+          <div className="md:w-1/2 md:ml-10">
+            <h1 className="text-2xl md:text-3xl font-semibold mb-2">
+              {selectedProduct.name}
+            </h1>
+            <p className="mb-1 text-lg text-gray-600 line-through">
+              {selectedProduct.originalPrice &&
+                `${selectedProduct.originalPrice}`}
+            </p>
+            <p className="text-gray-500 text-xl mb-2 ">
+              $ {selectedProduct.price}
+            </p>
+            <p className="text-gray-600 mb-4">{selectedProduct.description}</p>
+            <div className="mb-4">
+              <p className="text-gray-700">Color:</p>
+              <div className="flex gap-2 mt-2">
+                {selectedProduct.colors.map((color) => (
+                  <button
+                    key={color}
+                    className="border w-8 h-8 rounded-full"
+                    style={{
+                      backgroundColor: color.toLocaleLowerCase(),
+                      filter: "brightness(0.5)",
+                    }}
+                  ></button>
+                ))}
+              </div>
+            </div>
+
+            <div className="mb-4">
+              <p className="text-gray-700">Size:</p>
+              <div className="flex gap-2 mt-2">
+                {selectedProduct.sizes.map((size) => (
+                  <button key={size} className="border rounded px-4 py-2">
+                    {size}
+                  </button>
+                ))}
+              </div>
+            </div>
+
+            <div className="mb-6">
+              <p className="text-gray-700">Quantity:</p>
+              <div className="flex items-center space-x-4 mt-2">
+                <button className="rounded text-lg bg-gray-200 px-2 py-1">
+                  -
+                </button>
+                <span className="text-lg">1</span>
+                <button className="rounded text-lg bg-gray-200 px-2 py-1">
+                  +
+                </button>
+              </div>
+            </div>
+
+            <button className="bg-black text-white w-full rounded px-6 py-2">
+              Add To Cart
+            </button>
+
+            <div className="mt-10 text-gray-700">
+              <h3 className="font-bold text-xl mb-4">Characteristics:</h3>
+              <table className="w-full text-gray-600 text-left text-sm">
+                <tbody>
+                  <tr>
+                    <td className="py-1">Brand</td>
+                    <td className="py-1">{selectedProduct.brand}</td>
+                  </tr>
+                  <tr>
+                    <td className="py-1">Material</td>
+                    <td className="py-1">{selectedProduct.material}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ProductDetails;
