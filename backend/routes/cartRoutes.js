119f6ab02c55ae73376751c2a99cd4989281eb5a
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-18 12:51:02 +0530

    feat: Add update functionality to the cart

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -84,4 +84,41 @@ router.post("/", async (req, res) => {
   }
 });
 
+//@route PUT /api/cart
+//@desc Update product quantity in the cart for a guest or logged-in user
+//@access Public
+router.put("/", async (req, res) => {
+  const { productId, quantity, size, color, userId, guestId } = req.body;
+  try {
+    const cart = await getCart(userId, guestId);
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+    const productIndex = cart.products.findIndex(
+      (p) =>
+        p.productId.toString() === productId &&
+        p.color === color &&
+        p.size === size,
+    );
+    if (productIndex > -1) {
+      if (quantity > 0) {
+        cart.products[productIndex].quantity = quantity;
+      } else {
+        cart.products.splice(productIndex, 1);
+      }
+      cart.totalPrice = cart.products.reduce(
+        (acc, item) => acc + item.price * item.quantity,
+        0,
+      );
+      await cart.save();
+      return res.status(200).json(cart);
+    } else {
+      res.status(404).json({ message: "Product not found in cart" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
