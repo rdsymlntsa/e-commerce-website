02533d5ba13b49466b439f4ad437a8b6e9b6608c
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-20 00:45:12 +0530

    fix: Change paymentAddress to paymentMethod while extracting them. feat: Add put route to update checkout to mark as paid after successful payment.

--- backend/routes/checkoutRoutes.js
+++ backend/routes/checkoutRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 //@desc Create a checkout session
 //@access Private
 router.post("/", protect, async (req, res) => {
-  const { checkoutItems, shippingAddress, paymentAddress, totalPrice } =
+  const { checkoutItems, shippingAddress, paymentMethod, totalPrice } =
     req.body;
 
   if (!checkoutItems || checkoutItems.length === 0) {
@@ -32,6 +32,35 @@ router.post("/", protect, async (req, res) => {
     res.status(201).json(newCheckout);
   } catch (error) {
     console.log("Error creating checkout session: ", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//@route PUT /api/checkout/:id/pay
+//@desc Update checkout to mark as paid after successful payment
+//@access Private
+router.put("/:id/pay", protect, async (req, res) => {
+  const { paymentStatus, paymentDetails } = req.body;
+  try {
+    const checkout = await Checkout.findById(req.params.id);
+    if (!checkout) {
+      return res.status(404).json({ message: "checkout not found" });
+    }
+
+    if (paymentStatus === "paid") {
+      checkout.isPaid = true;
+      checkout.paymentMethod = paymentMethod;
+      checkout.paymentDetails = paymentDetails;
+      checkout.paidAt = Date.now();
+
+      await checkout.save();
+
+      res.status(200).json(checkout);
+    } else {
+      res.status(400).json({ message: "Invalid Payment Status" });
+    }
+  } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Server Error" });
   }
 });
