ba77a8f3842d6b2f9cec3b13893b88f8ac7b9630
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-22 03:40:51 +0530

    feat: Add subscribe route for subscriber model , which handle newsletter subscription

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const cartRoutes = require("./routes/cartRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
+const subscribeRoute = require("./routes/subscribeRoute");
 
 const app = express();
 app.use(express.json());
@@ -31,6 +32,7 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
+app.use("/api", subscribeRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
