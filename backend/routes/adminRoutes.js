56ec3210e21a34c42cb92c4db2b6e5d8c57110f9
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-22 04:26:42 +0530

    feat: Add update user for admin route

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -42,4 +42,23 @@ router.post("/", protect, admin, async (req, res) => {
   }
 });
 
+//@route PUT /api/admin/user/:id
+//@desc Update user info (admin only) - Name, email and role
+//@access Private/Admin
+router.put("/:id", protect, admin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.role = req.body.role || user.role;
+    }
+    const updatedUser = await user.save();
+    res.json({ message: "User updated successfully", user: updatedUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
