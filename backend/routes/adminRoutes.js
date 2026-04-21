42ac74cf36ba2aa35edb8e512202320bbad231f2
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-22 04:18:28 +0530

    feat: Add admin route to add new user (admin only)

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -17,4 +17,29 @@ router.get("/", protect, admin, async (req, res) => {
   }
 });
 
+//@route POST /api/admin/users
+//@desc Add a new user (admin only)
+//@access Private/Admin
+router.post("/", protect, admin, async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+      role: role || "customer",
+    });
+    await user.save();
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
 module.exports = router;
