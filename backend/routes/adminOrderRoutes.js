b110c8dd7c8facf23b2baa18683b4f76ad98363f
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-22 18:50:37 +0530

    feat: Add admin order route to delete an order

--- backend/routes/adminOrderRoutes.js
+++ backend/routes/adminOrderRoutes.js
@@ -31,9 +31,26 @@ router.put("/:id", protect, admin, async (req, res) => {
         req.body.status === "Delivered" ? Date.now() : order.deliveredAt;
       const updatedOrder = await order.save();
       res.json(updatedOrder);
+    } else {
+      res.status(404).json({ message: "Order not found" });
     }
-    else{
-      res.status(404).json({message: "Order not found"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//@route DELETE /api/admin/orders/:id
+//@desc Delete an order
+//@access Private/Admin
+router.delete("/:id", protect, admin, async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      await order.deleteOne();
+      res.json({ message: "Order removed" });
+    } else {
+      res.status(404).json({ message: "Order not found" });
     }
   } catch (error) {
     console.log(error);
