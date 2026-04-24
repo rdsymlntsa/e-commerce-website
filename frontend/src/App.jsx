729e9b849b49510f3df6da2444ab19b012dcab45
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-24 13:24:20 +0530

    feat: Add authSlice for handling authentication in the frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,38 +18,46 @@ import ProductManagement from "./components/Admin/ProductManagement";
 import EditProductPage from "./components/Admin/EditProductPage";
 import OrderManagement from "./components/Admin/OrderManagement";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Toaster position="top-right"></Toaster>
-      <Routes>
-        <Route path="/" element={<UserLayout />}>
-          <Route index element={<Home />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="register" element={<Register />}></Route>
-          <Route path="profile" element={<Profile />}></Route>
-          <Route
-            path="collections/:collection"
-            element={<CollectionPage />}
-          ></Route>
-          <Route path="product/:id" element={<ProductDetails />}></Route>
-          <Route path="checkout" element={<Checkout />}></Route>
-          <Route
-            path="order-confirmation"
-            element={<OrderConfirmationPage />}
-          ></Route>
-          <Route path="order/:id" element={<OrderDetailsPage />}></Route>
-          <Route path="my-orders" element={<MyOrdersPage />}></Route>
-        </Route>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<AdminHomePage />}></Route>
-          <Route path="users" element={<UserManagement />}></Route>
-          <Route path="products" element={<ProductManagement />}></Route>
-          <Route path="products/:id/edit" element={<EditProductPage />}></Route>
-          <Route path="orders" element={<OrderManagement />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Toaster position="top-right"></Toaster>
+        <Routes>
+          <Route path="/" element={<UserLayout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="register" element={<Register />}></Route>
+            <Route path="profile" element={<Profile />}></Route>
+            <Route
+              path="collections/:collection"
+              element={<CollectionPage />}
+            ></Route>
+            <Route path="product/:id" element={<ProductDetails />}></Route>
+            <Route path="checkout" element={<Checkout />}></Route>
+            <Route
+              path="order-confirmation"
+              element={<OrderConfirmationPage />}
+            ></Route>
+            <Route path="order/:id" element={<OrderDetailsPage />}></Route>
+            <Route path="my-orders" element={<MyOrdersPage />}></Route>
+          </Route>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminHomePage />}></Route>
+            <Route path="users" element={<UserManagement />}></Route>
+            <Route path="products" element={<ProductManagement />}></Route>
+            <Route
+              path="products/:id/edit"
+              element={<EditProductPage />}
+            ></Route>
+            <Route path="orders" element={<OrderManagement />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import register from "../assets/register.webp";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../redux/slices/authSlice";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name,setName]=useState("");
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("user registered: ",{name , email , password});
-  }
+    dispatch(registerUser({ name, email, password }));
+    //console.log("user registered: ",{name , email , password});
+  };
 
   return (
     <div className="flex">
       <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
-        <form onSubmit={handleSubmit} className="w-full max-w-md rounded-lg bg-white p-8 border shadow-sm">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md rounded-lg bg-white p-8 border shadow-sm"
+        >
           <div className="flex justify-center mb-6 ">
             <h2 className="font-medium text-xl">kalitego</h2>
           </div>
