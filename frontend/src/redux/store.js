ce5a8669774a12638f222341a58c8b04933373cd
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-25 09:44:26 +0530

    feat: Add slice for cart

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/productsSlice";
+import cartReducer from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
+    cart: cartReducer,
   },
 });
 
