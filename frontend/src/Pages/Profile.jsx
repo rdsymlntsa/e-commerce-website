b52fb7d84fe9a2902d74bca28a65571658cb94e3
rdsymlntsa <rdsymlntsa@gmail.com>
2026-04-27 13:30:22 +0530

    feat: Integrate login , logout and protected profile route

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -1,14 +1,40 @@
 import MyOrdersPage from "./MyOrdersPage";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../redux/slices/authSlice";
+import { clearCart } from "../redux/slices/cartSlice";
+import { useEffect } from "react";
 
 const Profile = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(clearCart());
+    navigate("/login");
+  };
   return (
     <div className="min-h-screen flex flex-col">
       <div className="container mx-auto  grow p-4 md:p-6">
         <div className="flex flex-col md:flex-row md:space-x-4 space-y-6 md:space-y-0">
           <div className="w-full md:w-1/3 lg:w-1/4 rounded-lg p-6 shadow-md">
-            <h1 className="text-2xl md:text-3xl font-bold mb-4">John Doe</h1>
-            <p className="text-lg text-gray-600 mb-4">johndoe@example.com</p>
-            <button className="bg-red-500 text-white hover:text-red-600 w-full rounded px-4 py-2">
+            <h1 className="text-2xl md:text-3xl font-bold mb-4">
+              {user?.name}
+            </h1>
+            <p className="text-lg text-gray-600 mb-4">{user?.email}</p>
+            <button
+              onClick={handleLogout}
+              className="bg-red-500 text-white hover:text-red-600 w-full rounded px-4 py-2"
+            >
               Logout
             </button>
           </div>
